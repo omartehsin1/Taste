f2149d65bc496ac42dbe526df8e9e04108c3b1c1
Omar Tehsin <omartehsin@gmail.com>
2019-01-31 12:32:11 -0500

    Added collection view images

--- Taste/ViewController.m
+++ Taste/ViewController.m
@@ -19,7 +19,7 @@
 @interface ViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
 @property (weak, nonatomic) IBOutlet UICollectionView* foodCollectionVC;
 @property (nonatomic) NSArray<Recipe*> * recepiesData;
-@property (nonatomic) NSMutableArray* recepies;
+@property (nonatomic) NSMutableArray* recepiesArray;
 @property (nonatomic) NSString* search;
 //@property (nonatomic, strong) ColourAnimator *colourAnimator;
 @property (weak, nonatomic) IBOutlet UIView *backgroundview;
@@ -30,15 +30,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    int backgroundLoop = 0;
-    ColourAnimator *colourAnimator = [[ColourAnimator alloc]init];
-    //self.backgroundview.backgroundColor = [UIColor orangeColor];
-//    while (backgroundLoop < colourAnimator.colourWheel.count - 1) {
-//        backgroundLoop++;
-//        [UIView animateWithDuration:2.5 animations:^{
-//            self.textDisplayView.backgroundColor = [colourAnimator colourGenerator];
-//        } completion:NULL];
-//    }
     self.foodCollectionVC.dataSource = self;
     self.foodCollectionVC.delegate = self;
     [self fetchData];
@@ -49,8 +40,10 @@
     if (self.search == nil) {
         self.search = @"chicken";
     }
-    NSString* inPutUrl = [NSString stringWithFormat: @"https://api.edamam.com/search?q=%@&app_id=ecacded3&app_key=935ae12374e7cbb6f82dc1b513aa7dbb", self.search];
-    NSURL* url = [NSURL URLWithString:inPutUrl];
+    //NSString *inPutUrl = [NSString stringWithFormat:@"https://www.food2fork.com/api/search?key=29f2a594050bcf25be3fd8071f18924d&q=chicken%20breast&page=2", self.search];
+    //NSString *inPutUrl = @"https://www.food2fork.com/api/search?key=29f2a594050bcf25be3fd8071f18924d&q=chicken%20breast&page=2";
+    NSURL *url = [NSURL URLWithString:@"https://www.food2fork.com/api/search?key=29f2a594050bcf25be3fd8071f18924d&q=chicken%20breast&page=2"];
+    //NSURL* url = [NSURL URLWithString:inPutUrl];
     NSURLRequest* request = [NSURLRequest requestWithURL: url];
     NSURLSessionTask* task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^
                               (NSData * _Nullable data,
@@ -58,17 +51,20 @@
                                NSError * _Nullable error) {
                                   NSError* jsonError;
                                   NSDictionary* json = [NSJSONSerialization JSONObjectWithData:data options: 0 error: &jsonError];
-                                  NSArray* recipeObjects = json[@"hits"][@"recipe"];
-                                  self.recepies = [[NSMutableArray alloc]init];
+                                  NSArray* recipeObjects = json[@"recipes"];
+                                  self.recepiesArray = [[NSMutableArray alloc]init];
                                   for (NSDictionary* recipeDictionary in recipeObjects){
-                                      Recipe* aRecipe = [Recipe fromJsonDictionary:recipeDictionary];
-                                      [self.recepies addObject:aRecipe];
+                                      //Recipe* aRecipe = [Recipe fromJsonDictionary:recipeDictionary];
+                                      Recipe *aRecipe = [[Recipe alloc]initWithJsonDictionary:recipeDictionary];
+                                      [self.recepiesArray addObject:aRecipe];
                                   }
-                                  self.recepiesData = self.recepies;
+                                  self.recepiesData = self.recepiesArray;
                                   [NSOperationQueue.mainQueue addOperationWithBlock:^{
                                       [self.foodCollectionVC reloadData];
                                   }];
-                              }];
+     
+                                  }];
+    
     [task resume];
 }
 
@@ -78,11 +74,17 @@
     RecipeCollectionViewCell* cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"recipeCell" forIndexPath:indexPath];
     cell.tag = indexPath.item;
     cell.recipe = self.recepiesData[indexPath.item];
+    [cell.recipe loadImage];
     
     return cell;
 }
 
 -(void) animateBackgroundColour {
+    ColourAnimator *colourAnimator = [[ColourAnimator alloc]init];
+    self.backgroundview.backgroundColor = [UIColor orangeColor];
+    [UIView animateWithDuration:2.5 animations:^{
+        self.textDisplayView.backgroundColor = [colourAnimator colourGenerator];
+    } completion:NULL];
     
 }
 
